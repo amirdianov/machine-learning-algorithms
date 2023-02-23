ecb778cb61a9d8539dcd8d78ae65ca4a44a79a2b
diyanovamir <amirdianov@gmail.com>
2023-02-23 19:36:22 +0300

    + mse

--- utils/metrics.py
+++ utils/metrics.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 def MSE(predictions: np.ndarray, targets: np.ndarray) -> float:
-    """ Todo calculate loss of your model without loops"""
-    pass
+    """Todo calculate loss of your model without loops"""
+    return sum((predictions - targets) ** 2) / len(targets)
