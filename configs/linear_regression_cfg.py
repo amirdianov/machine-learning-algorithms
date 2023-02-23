8188b0f077c8fe1f435531d9314fbb0479418a57
diyanovamir <amirdianov@gmail.com>
2023-02-23 23:34:29 +0300

    = count func

--- configs/linear_regression_cfg.py
+++ configs/linear_regression_cfg.py
@@ -5,5 +5,4 @@ from easydict import EasyDict
 cfg = EasyDict()
 cfg.dataframe_path = os.path.basename("linear_regression_dataset.csv")
 # TODO list of basis functions
-
-cfg.base_functions = [lambda x, degree=i: x**degree for i in range(1000)]
+cfg.base_functions = [lambda x, degree=i: x**degree for i in range(8)]
