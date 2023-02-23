87d4c631f56a9fbab0c6ea50902fa2e9ef29eb6a
diyanovamir <amirdianov@gmail.com>
2023-02-23 19:31:47 +0300

    + linter

--- datasets/linear_regression_dataset.py
+++ datasets/linear_regression_dataset.py
@@ -1,10 +1,12 @@
 import numpy as np
+
 from utils.common_functions import read_dataframe_file
 
-class LinRegDataset():
 
-    def __call__(self, dataframe_path:str)->dict:
+class LinRegDataset:
+    def __call__(self, dataframe_path: str) -> dict:
         advertising_dataframe = read_dataframe_file(dataframe_path)
-        return {'inputs': np.asarray(advertising_dataframe['inputs']),
-                'targets': np.asarray(advertising_dataframe['targets'])}
-
+        return {
+            "inputs": np.asarray(advertising_dataframe["inputs"]),
+            "targets": np.asarray(advertising_dataframe["targets"]),
+        }
--- execute.py
+++ execute.py
@@ -1,5 +1,5 @@
-from models.linear_regression_model import LinearRegression
 from datasets.linear_regression_dataset import LinRegDataset
+from models.linear_regression_model import LinearRegression
 from utils.metrics import MSE
 from utils.visualisation import Visualisation
 
@@ -8,16 +8,19 @@ def experiment(lin_reg_cfg, visualise_prediction=True):
     lin_reg_model = LinearRegression(lin_reg_cfg.base_functions)
     linreg_dataset = LinRegDataset()(lin_reg_cfg.dataframe_path)
 
-    predictions = lin_reg_model(linreg_dataset['inputs'])
-    error = MSE(predictions, linreg_dataset['targets'])
+    predictions = lin_reg_model(linreg_dataset["inputs"])
+    error = MSE(predictions, linreg_dataset["targets"])
 
     if visualise_prediction:
-        Visualisation.visualise_predicted_trace(predictions,
-                                                linreg_dataset['inputs'],
-                                                linreg_dataset['targets'],
-                                                plot_title=f'Полином степени {len(lin_reg_cfg.base_functions)}; MSE = {error}')
+        Visualisation.visualise_predicted_trace(
+            predictions,
+            linreg_dataset["inputs"],
+            linreg_dataset["targets"],
+            plot_title=f"Полином степени {len(lin_reg_cfg.base_functions)}; MSE = {error}",
+        )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     from configs.linear_regression_cfg import cfg as lin_reg_cfg
-    experiment(lin_reg_cfg,visualise_prediction=True)
+
+    experiment(lin_reg_cfg, visualise_prediction=True)
