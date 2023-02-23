3e6370fe0e5cf195e33c6fa9e0c5c8b25a09d369
diyanovamir <amirdianov@gmail.com>
2023-02-23 22:04:13 +0300

    + visualization

--- utils/visualisation.py
+++ utils/visualisation.py
@@ -1,10 +1,14 @@
-import plotly.graph_objects as go
 import numpy as np
+import plotly
+import plotly.graph_objects as go
+from plotly.graph_objs import Layout
 
-class Visualisation():
 
+class Visualisation:
     @staticmethod
-    def visualise_predicted_trace(prediction:np.ndarray,inputs:np.ndarray,targets:np.ndarray,plot_title=''):
+    def visualise_predicted_trace(
+        prediction: np.ndarray, inputs: np.ndarray, targets: np.ndarray, plot_title=""
+    ):
         # TODO visualise predicted trace and targets
         """
 
@@ -13,7 +17,12 @@ class Visualisation():
         :param targets: target variables (oy for one trace)
         :param plot_title: plot title
         """
-        pass
+        trace1 = go.Scatter(x=inputs, y=targets, mode="markers", name="target")
+        trace2 = go.Scatter(x=inputs, y=prediction, mode="lines", name="prediction")
+
+        plotly.offline.plot(
+            {"data": [trace1, trace2], "layout": Layout(title=plot_title)}
+        )
 
     @staticmethod
     def visualise_error():
