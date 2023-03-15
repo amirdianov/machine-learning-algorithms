ab2216523d3871ead4c0f186f3529f78910971c6
diyanovamir <amirdianov@gmail.com>
2023-03-15 19:44:14 +0300

    +started doing hw

--- execute.py
+++ execute.py
@@ -1,10 +1,5 @@
 from dataset.sportsmans_height import Sportsmanheight
 from model.simple_classifier import Classifier
-import numpy as np
-import pandas as pd
-from config.cfg import cfg
-import plotly.graph_objects as go
-import copy
 
 dataset = Sportsmanheight()()
 predictions = Classifier()(dataset['height'])
