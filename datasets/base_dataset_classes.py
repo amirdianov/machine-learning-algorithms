0e6eaddef0732ae3c98696329098df70e34cb955
diyanovamir <amirdianov@gmail.com>
2023-03-24 23:43:39 +0300

    + divide method

--- datasets/base_dataset_classes.py
+++ datasets/base_dataset_classes.py
@@ -1,6 +1,8 @@
 from abc import ABC, abstractmethod
 
 import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
 
 
 class BaseDataset(ABC):
@@ -28,10 +30,24 @@ class BaseDataset(ABC):
         pass
 
     def divide_into_sets(self):
-        # TODO define self.inputs_train, self.targets_train, self.inputs_valid, self.targets_valid,
+        # define self.inputs_train, self.targets_train, self.inputs_valid, self.targets_valid,
         #  self.inputs_test, self.targets_test; you can use your code from previous homework
 
-        pass
+        df = pd.DataFrame(
+            np.concatenate((self.inputs.reshape(-1, 1), self.targets.reshape(-1, 1)), axis=1),
+            columns=["inputs", "targets"],
+        )
+        df_shuffled = df.sample(frac=1)
+        inputs_shuffled = df_shuffled["inputs"]
+        targets_shuffled = df_shuffled["targets"]
+        self.inputs_train, x_memory, self.targets_train, y_memory = train_test_split(
+            inputs_shuffled, targets_shuffled, train_size=self.train_set_percent
+        )
+        self.inputs_valid, self.inputs_test, self.targets_valid, self.targets_test = train_test_split(
+            x_memory,
+            y_memory,
+            test_size=round(self.valid_set_percent * 100 / (1 - self.train_set_percent)) / 100,
+        )
 
     def normalization(self):
         # TODO write normalization method BONUS TASK
