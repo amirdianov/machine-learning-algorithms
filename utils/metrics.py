9cead5c8502f7b2ce1f560fc0cad67faac222a0b
diyanovamir <amirdianov@gmail.com>
2023-03-26 16:17:16 +0300

    + matrix

--- utils/metrics.py
+++ utils/metrics.py
@@ -1,4 +1,5 @@
 import numpy as np
+from sklearn.metrics import confusion_matrix
 
 
 def MSE(predictions: np.ndarray, targets: np.ndarray) -> float:
@@ -9,6 +10,7 @@ def accuracy(predictions: np.ndarray, targets: np.ndarray) -> float:
     # TODO calculate accuracy
     pass
 
-def confusion_matrix(*args, **kwargs):
+
+def conf_matrix(targets, model_confidence):
     # TODO build confusion matrix
-    pass
+    return confusion_matrix(list(targets), list(model_confidence))
