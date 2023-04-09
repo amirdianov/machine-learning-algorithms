b083c7bb74915d4133ff086b3eca4b3f32f1cf3e
diyanovamir <amirdianov@gmail.com>
2023-04-09 11:58:41 +0300

    + main task init

--- utils/metrics.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-
-def MSE(predictions: np.ndarray, targets: np.ndarray) -> float:
-    """ Calculate loss of your model without loops. Last homework, if you haven't done it, ypu need to do it anyway"""
-    pass
