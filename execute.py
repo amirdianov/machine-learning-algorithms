8dd1ede009ddedf42a9834126a83b92c866aa86d
diyanovamir <amirdianov@gmail.com>
2023-04-28 01:16:33 +0300

    + metrics

--- execute.py
+++ execute.py
@@ -1,6 +1,6 @@
 from adaboost_students import Adaboost
 from dataset_titanik import Titanic
-from utils.metrics import conf_matrix
+from utils.metrics import conf_matrix, count_section_metrics
 
 N = 30
 model = Adaboost(N)
@@ -8,18 +8,4 @@ train_test_data = Titanic('titanik_train_data.csv', 'titanik_test_data.csv')()
 model.train(train_test_data['train_target'], train_test_data['train_input'])
 predict = model.get_prediction(train_test_data['test_input'])
 print(conf_matrix(predict, train_test_data['test_target']))
-# model
-# des_tr = DT(SetTypeOfTask.classification.name)
-# des_tr.train(des_tr[], train['targets'])
-# predictions_valid = des_tr.get_predictions(valid['inputs'])
-# accuracy_valid = accuracy(predictions_valid, valid['targets'])
-# conf_matrix_valid = conf_matrix(predictions_valid, valid['targets'])
-# predictions_test = des_tr.get_predictions(test['inputs'])
-# accuracy_test = accuracy(predictions_test, test['targets'])
-# conf_matrix_test = conf_matrix(predictions_test, test['targets'])
-# print(accuracy_valid, conf_matrix_valid, accuracy_test, conf_matrix_test)
-#
-# des_tr = DT(SetTypeOfTask.regression.name)
-# des_tr.train(train['inputs'], train['targets'])
-# predictions_valid = des_tr.get_predictions(valid['inputs'])
-# print(MSE(predictions_valid, valid['targets']))
+print(count_section_metrics(train_test_data['test_target'], predict))
--- utils/metrics.py
+++ utils/metrics.py
@@ -18,3 +18,32 @@ def accuracy(predictions: np.ndarray, targets: np.ndarray) -> float:
 def conf_matrix(predict, targets):
     # build confusion matrix
     return confusion_matrix(list(targets), list(predict))
+
+
+def count_metrics(metrics):
+    TP, TN, FP, FN = metrics[(1, 1)], metrics[(-1, -1)], metrics[(-1, 1)], metrics[(1, -1)]
+    metrics['accuracy'] = (TP + TN) / (TP + TN + FP + FN)
+    metrics['precision'] = TP / (TP + FP)
+    metrics['recall'] = TP / (TP + FN)
+    metrics['f1_score'] = 2 * (metrics['precision'] * metrics['recall']) / (
+            metrics['precision'] + metrics['recall']) if metrics['precision'] + metrics['recall'] != 0 else 0
+    metrics['a'] = FP / (TN + FP)
+    metrics['b'] = FN / (TP + FN)
+    return metrics
+
+
+def count_section_metrics(gt, predict):
+    metrics = {
+        # TrueNeg
+        (-1, -1): 0,
+        # FalsePos
+        (-1, 1): 0,
+        # FalseNeg
+        (1, -1): 0,
+        # TruePos
+        (1, 1): 0
+    }
+    df = np.column_stack((gt, predict))
+    for pair in df:
+        metrics[(pair[0], pair[1])] += 1
+    return count_metrics(metrics)
